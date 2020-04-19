42d4a9e957127e9cdb1d838d2e80f266c49a952e
shazrobot <33558832+shazrobot@users.noreply.github.com>
2020-04-19 17:08:09 +1200

    Visuals upgraded

--- Ludum Dare 46 - Systematic/Assets/Scripts/PlayerMovement.cs
+++ Ludum Dare 46 - Systematic/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody Character;
     public double AngleFromX;
     public double MoveForce;
+    
     public double TurnRate;
+    private double TrueMoveForce;
 
     public double DegreesToRadians(double degrees)
     {
@@ -18,16 +20,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(Input.GetKey("left shift"))
+        {
+            TrueMoveForce = MoveForce * 2;
+        }
+        else
+        {
+            TrueMoveForce = MoveForce;
+        }
         AngleFromX = Character.transform.rotation.eulerAngles.y;
         if (Input.GetKey("w")) {
-            Character.AddForce(-transform.right*(float)MoveForce * Time.deltaTime);
+            Character.AddForce(-transform.right*(float)TrueMoveForce * Time.deltaTime);
         }
         if (Input.GetKey("a")) {
             Vector3 Rotation = new Vector3(0, -(float)TurnRate*Time.deltaTime, 0);
             Character.transform.Rotate(Rotation);
         }
         if (Input.GetKey("s")) {
-            Character.AddForce(transform.right*(float)MoveForce*Time.deltaTime);
+            Character.AddForce(transform.right*(float)TrueMoveForce * Time.deltaTime);
 
         }
         if (Input.GetKey("d")) {
@@ -35,10 +45,10 @@ public class PlayerMovement : MonoBehaviour
             Character.transform.Rotate(Rotation);
         }
         if (Input.GetKey("q")) {
-            Character.AddForce(-transform.forward * (float)MoveForce * Time.deltaTime);
+            Character.AddForce(-transform.forward * (float)TrueMoveForce * Time.deltaTime);
         }
         if (Input.GetKey("e")) {
-            Character.AddForce(transform.forward * (float)MoveForce * Time.deltaTime);
+            Character.AddForce(transform.forward * (float)TrueMoveForce * Time.deltaTime);
         }
 
             Character.freezeRotation = true;
